f7008efdb3b7c3388cd0600b23bed98b0c270646
Vinicius Luiz de Souza Reis <viniciusluizdesouzareis@pop-os.localdomain>
2023-03-24 14:23:26 -0300

    Inicio de projeto e requisito 1

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,5 +1,6 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
@@ -16,7 +17,9 @@ def read(path: str) -> List[Dict]:
     list
         List of rows as dicts
     """
-    raise NotImplementedError
+    with open(path) as jobs_file:
+        data = list(csv.DictReader(jobs_file))
+        return data
 
 
 def get_unique_job_types(path: str) -> List[str]:
