55d7157d199351c791197c81c28239269400edfe
Vinicius Luiz de Souza Reis <viniciusluizdesouzareis@pop-os.localdomain>
2023-03-24 14:50:36 -0300

    fet: implement get_unique_job_types (Requisito 2)

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -37,7 +37,12 @@ def get_unique_job_types(path: str) -> List[str]:
     list
         List of unique job types
     """
-    raise NotImplementedError
+    data = read(path)
+    jobs = list()
+    for job in data:
+        if job["job_type"] not in jobs:
+            jobs.append(job["job_type"])
+    return jobs
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
